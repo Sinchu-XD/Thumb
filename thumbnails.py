47456a18853b60e82759d2f4d5cc8d2701a4f927
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-26 18:17:21 +0530

    Update thumbnails.py

--- thumbnails.py
+++ thumbnails.py
@@ -307,18 +307,17 @@ async def get_thumb(title, duration, thumbnail, channel=None, views=None, videoi
             shadow_blur=3
         )
         
-        # Song title - line 1
-# Song title (safe rendering)
-     for i, line in enumerate(title1):
-    draw_text_with_shadow(
-        background, draw,
-        (text_x, title_y + (i * 60)),
-        line,
-        title_font,
-        (255, 255, 255),
-        shadow_offset=(3, 3),
-        shadow_blur=6
-    )
+        # Song title (safe rendering)
+        for i, line in enumerate(title1):
+            draw_text_with_shadow(
+                background, draw,
+                (text_x, title_y + (i * 60)),
+                line,
+                title_font,
+                (255, 255, 255),
+                shadow_offset=(3, 3),
+                shadow_blur=6
+            )
         
         # Artist/Channel info with icon
         artist_y = title_y + 140
